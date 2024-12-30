7cfb0fb2269c4dda291f72423e75a398e6e01037
Arriisu <aris.vucu@student.upt.ro>
2024-12-30 18:17:54 +0200

    Added login/logout

---
+++ ProiectPOO/ManagementUtilizatori.cs
@@ -0,0 +1,37 @@
+namespace proiectPOO;
+
+public class ManagementUtilizatori
+{
+    private Dictionary<String, String> Utilizatori = new Dictionary<string, string>();
+
+    public Dictionary<String, String> returneazaUtilizatori()
+    {
+        return Utilizatori;
+    }
+    
+    public void citireUtilizatori()
+    {
+        try
+        {
+            using StreamReader reader = new("../../../utilizatori.txt");
+            
+            string ln;
+
+            while ((ln = reader.ReadLine()) != null) {
+                // Console.WriteLine(ln);
+                String[] splited = ln.Split(" ");
+                Utilizatori.Add(splited[0], splited[1]);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void adaugareUtilizator(Utilizator u)
+    {
+        Utilizatori.Add(u.getEmail(), u.getParola());
+    }
+}
--- ProiectPOO/Utilizator.cs
+++ ProiectPOO/Utilizator.cs
@@ -23,4 +23,35 @@ public class Utilizator
     {
         return CodUnic + " " + Nume + " " + Prenume + " " + Email + " " + Parola;
     }
+    
+    public String getEmail()
+    {
+        return this.Email;
+    }
+    
+    public String getParola()
+    {
+        return this.Parola;
+    }
+    
+    public void Login(ManagementUtilizatori mu)
+    {
+        Dictionary<String, String> utilizatori = mu.returneazaUtilizatori();
+        foreach (KeyValuePair<string, string> kvp in utilizatori)
+        {
+            if (this.Email == kvp.Key && this.Parola == kvp.Value)
+            {
+                this.Logat = true;
+                Console.WriteLine("Logat cu succes");
+            }
+        }
+        if (this.Logat == false)
+            Console.WriteLine("Logat fara succes");
+    }
+
+    public void Logout()
+    {
+        Console.WriteLine("Delogat cu succes");
+        this.Logat = false;
+    }
 }
